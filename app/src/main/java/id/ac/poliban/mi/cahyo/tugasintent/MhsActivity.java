86b098fc9dd6d4dce05655a2f559720838dcdc54
cahyo098 <cahyopkr@gmail.com>
2020-02-05 12:59:13 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/cahyo/tugasintent/MhsActivity.java
@@ -0,0 +1,33 @@
+package id.ac.poliban.mi.cahyo.tugasintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MhsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mhs);
+
+        EditText edNim = findViewById(R.id.edNim);
+        EditText edNama = findViewById(R.id.edNama);
+        EditText edAlamat = findViewById(R.id.edAlaamt);
+        EditText edTelp = findViewById(R.id.edTelp);
+        Button btSend = findViewById(R.id.btSend);
+
+        btSend.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.putExtra("MhsNim", edNim.getText().toString());
+            intent.putExtra("MhsNama", edNama.getText().toString());
+            intent.putExtra("MhsAlamat", edAlamat.getText().toString());
+            intent.putExtra("MhsTelp", edTelp.getText().toString());
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+    }
+}
